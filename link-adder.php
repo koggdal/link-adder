ba716bd987ae478492536b6a0b7da2d74f4aba39
Johannes Koggdal <johannes@koggdal.com>
2010-12-28 18:26:34 +0100

    Removed version number from JS files, since they are never actually cached. The content of the files are fetched from the PHP files and then outputted to the user.

--- link-adder.php
+++ link-adder.php
@@ -24,14 +24,14 @@ function link_adder_settings()
 	if (!current_user_can('manage_options'))
 		wp_die( __('You do not have sufficient permissions to access this page.') );
 		
-	$code = file_get_contents(WP_PLUGIN_URL."/link-adder/link-adder.1.0.1.min.js");
+	$code = file_get_contents(WP_PLUGIN_URL."/link-adder/link-adder.min.js");
 	$code = str_replace("%SITENAME%",get_bloginfo('name'),$code);
 	$code = htmlentities($code);
 	$code = preg_replace("/\/\*.*?\*\/\n/s","javascript:",$code);
 	$code = preg_replace("/plugin_folder:'.*?'/","plugin_folder:'".WP_PLUGIN_URL."/link-adder/'",$code);
 	$code = str_replace("hash:''","hash:'".get_option('link-adder-id')."'",$code);
 	
-	$code_full = file_get_contents(WP_PLUGIN_URL."/link-adder/link-adder.1.0.1.js");
+	$code_full = file_get_contents(WP_PLUGIN_URL."/link-adder/link-adder.js");
 	$code_full = str_replace("%SITENAME%",get_bloginfo('name'),$code_full);
 	$code_full = htmlentities($code_full);
 	$code_full = preg_replace("/plugin_folder: '.*?'/","plugin_folder: '".WP_PLUGIN_URL."/link-adder/'",$code_full);
